d9616d082dfd27f6afa85b29c8dddfe37ef413c7
tom <tom@that-place>
2024-07-15 05:55:59 -0500

    Using new hazeldemo with packing.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [hazeldemo "0.1.0-SNAPSHOT"
+                 [hazeldemo "0.1.1-SNAPSHOT"
                   :exclusions [spork/spork]]
                  [marathon  "4.2.18-SNAPSHOT"]
                  [com.hazelcast/hazelcast-aws "3.4"]]
